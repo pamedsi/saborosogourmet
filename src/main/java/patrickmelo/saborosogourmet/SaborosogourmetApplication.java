0d0300568dfae71c9a1cfef0804e51d9ee1aca9c
Patrick Melo <patrickabimael@yahoo.com.br>
2023-07-27 20:27:17 -0300

    Primeiro commit

---
+++ src/main/java/patrickmelo/saborosogourmet/SaborosogourmetApplication.java
@@ -0,0 +1,13 @@
+package patrickmelo.saborosogourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborosogourmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaborosogourmetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/patrickmelo/saborosogourmet/SaborosogourmetApplicationTests.java
@@ -0,0 +1,13 @@
+package patrickmelo.saborosogourmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaborosogourmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
